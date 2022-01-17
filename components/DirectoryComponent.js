7646992da00314fe92d21fa58a6ec9bd1c081bb5
Toddj <todd.j.marino@gmail.com>
2022-01-17 14:35:52 -0600

    Components Part 1

---
+++ components/DirectoryComponent.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FlatList } from 'react-native'
+import { ListItem } from 'react-native-elements'
+
+function Directory(props) {
+
+    const renderDirectoryItem = ( {item} ) => {
+        return (
+            <ListItem
+                title={item.name}
+                subtitle={(item.description)}
+                // LeftAvatar requires an object. 
+                // First braces to embed javascript. 
+                // Second to define the object literal
+                leftAvatar={ { source: require('./images/react-lake.jpg')} }
+            />
+        );
+    };
+
+    return (
+        <FlatList
+            data={props.campsites}
+            renderItem={renderDirectoryItem}
+            keyExtractor={item => item.id.toString()}
+        />
+    )
+
+}
+
+export default Directory
