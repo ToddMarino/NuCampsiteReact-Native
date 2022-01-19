077076d7efb042ba0ae1d570f65f706df5ee87d1
Toddj <todd.j.marino@gmail.com>
2022-01-19 17:40:11 -0600

    Week1:3

--- components/ContactComponents.js
+++ components/ContactComponents.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { FlatList } from 'react-native';
 import { Card, Text } from 'react-native-elements'
 import { ScrollView } from 'react-native-gesture-handler';
 
@@ -13,13 +12,13 @@ static navigationOptions = {
         return (
             <ScrollView>
                 <Card 
+                title={'Contact Information'}
                 wrapperStyle={{ margin: 20 }}
-                title={'Contact Information'}>
+                >
                     <Text>1 Nucamp Way</Text>
                     <Text>Seattle, WA 98001</Text>
                     <Text
                         style={{ marginBottom: 10 }}>U.S.A.</Text>
-
                     <Text>Phone: 1-206-555-1234</Text>
                     <Text>Email: campsites@nucamp.co</Text>
                 </Card>
