4ea1cac66ed620a0ef7524420a6321251851f529
ToddMarino <todd.j.marino@gmail.com>
2022-02-06 12:06:56 -0600

    static IP address

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,5 +1,5 @@
 // laptop at home
-export const baseUrl = 'http://192.168.4.54:3001/';
+export const baseUrl = 'http://192.168.4.200:3001/';
 
 // desktop at home
 // export const baseUrl = 'http://192.168.0.219:3001/'
