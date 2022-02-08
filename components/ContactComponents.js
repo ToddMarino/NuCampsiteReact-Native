83ae96805ba167af59717e4171b4c62292464f7f
ToddMarino <todd.j.marino@gmail.com>
2022-02-08 12:23:04 -0600

    Sending Email
    Exercise completed. Notifications still not working and form not resetting. Awaiting help from Slack.

--- components/ContactComponents.js
+++ components/ContactComponents.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
-import { Card, Text } from 'react-native-elements'
-import { ScrollView } from 'react-native-gesture-handler';
 import * as Animatable from 'react-native-animatable'
+import { Card, Button, Icon, Text } from 'react-native-elements'
+import { ScrollView } from 'react-native-gesture-handler';
+import * as MailComposer from 'expo-mail-composer';
+;
+
 
 class Contact extends Component {
 
 static navigationOptions = {
     title: 'Contact Us'
+}
+
+sendMail() {
+    MailComposer.composeAsync({
+        recipients: ['campsites@nucamp.co'],
+        subject: 'Inquiry',
+        body: 'To whom it may concern:'
+    })
 }
 
     render() {
@@ -23,6 +34,17 @@ static navigationOptions = {
                             style={{ marginBottom: 10 }}>U.S.A.</Text>
                         <Text>Phone: 1-206-555-1234</Text>
                         <Text>Email: campsites@nucamp.co</Text>
+                        <Button 
+                            title="Send Email"
+                            buttonStyle={{backgroundColor: '#5637dd', margin: 40}}
+                            icon={<Icon 
+                                name='envelope-o'
+                                type='font-awesome'
+                                color="#fff"
+                                iconStyle={{marginRight: 10}}
+                            />}
+                            onPress={() => this.sendMail()}
+                        />
                     </Card>
                 </Animatable.View>
             </ScrollView>
